445a67d1c84f8d3300608d72c3dac0c90def500f
Star-Owl <hasiradoo@starowl.dev>
2024-03-26 21:19:27 +0100

    fixed meta image

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -25,10 +25,10 @@ const MetaTags = ({
       content={description}
     />
 
-    <meta property="og:image" key="og:image" content="/images/og.png" />
+    <meta property="og:image" key="og:image" content="/og.png" />
     <meta property="og:type" key="og:type" content="website" />
     <meta property="og:site_name" key="og:site_name" content="Kris German" />
-    <meta property="og:url" key="og:url" content="/images/og.png" />
+    <meta property="og:url" key="og:url" content="/og.png" />
     <meta
       name="twitter:image:alt"
       key="twitter:image:alt"
