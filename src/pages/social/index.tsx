558f9b2e4871f68d53ab10f786c0acff894248a9
Star-Owl <hasiradoo@starowl.dev>
2024-04-12 14:28:26 +0200

    added social index

---
+++ src/pages/social/index.tsx
@@ -0,0 +1,13 @@
+import Layout from 'components/social/Layout'
+import Post from 'components/social/Post' // Fixed import
+import React from 'react'
+
+const Page = () => {
+  return (
+    <Layout>
+      <Post />
+    </Layout>
+  )
+}
+
+export default Page
