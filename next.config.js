45fef84a0fe126764fd27cf21b825c45e2a11de3
Star-Owl <hasiradoo@starowl.dev>
2024-04-13 12:29:00 +0200

    added domains to config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['kris.starowl.social', 'pbs.twimg.com'],
+  },
 }
 
 module.exports = nextConfig
