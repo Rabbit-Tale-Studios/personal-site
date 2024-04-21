882ff0b6b4175c4dcf28e241ff04cb3593d0cbef
Star-Owl <hasiradoo@starowl.dev>
2024-04-21 18:52:33 +0200

    added UserData and refactored code

--- src/components/social/Layout.tsx
+++ src/components/social/Layout.tsx
@@ -10,6 +10,40 @@ import React from 'react'
 import LeftNav from './LeftNav'
 import Aside from './Aside'
 
+const users = [
+  {
+    id: 1,
+    displayName: 'Hasira 🥃🪴',
+    username: 'hasiradoo',
+    bio: 'Full-stack developer, @getnextui lover she/her 🎉',
+    avatarUrl:
+      'https://pbs.twimg.com/profile_images/1777614638261080064/H9iQD24q_400x400.jpg',
+    coverUrl: 'https://i.pravatar.cc/150?u=a04258114e29026702e',
+    followers: [
+      {
+        id: 2,
+      },
+    ],
+    following: [],
+    stories: [],
+  },
+  {
+    id: 2,
+    displayName: 'Jane Doe',
+    username: 'janedoe',
+    bio: 'Full-stack developer, @getnextui lover she/her 🎉',
+    avatarUrl: 'https://i.pravatar.cc/150?u=a04258114e29026702e',
+    coverUrl: 'https://i.pravatar.cc/150?u=a04258114e29026702f',
+    followers: [],
+    following: [
+      {
+        id: 1,
+      },
+    ],
+    stories: [],
+  },
+]
+
 function ThemeWatcher() {
   let { resolvedTheme, setTheme } = useTheme()
 
@@ -38,15 +72,22 @@ const Layout = (props: {
   children?: React.ReactNode
   metaTagsProps?: React.ComponentProps<typeof MetaTags>
 }) => {
+  const user = users[0]
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       <FontProvider>
         <MetaTags {...(props.metaTagsProps ?? {})} />
         <ThemeWatcher />
         <NextUIProvider>
-          <div className="flex bg-shark-50 lg:justify-center xl:gap-6">
-            <LeftNav />
-            <main className="flex w-full flex-col leading-none sm:space-y-4 md:max-w-xl md:p-0 md:py-[2.5rem] lg:max-w-lg xl:max-w-xl">
+          <div className="flex lg:justify-center xl:gap-6">
+            <LeftNav
+              displayName={user.displayName}
+              username={user.username}
+              avatarUrl={user.avatarUrl}
+              stories={user.stories}
+            />
+            <main className="flex w-full flex-col leading-none sm:space-y-2 md:max-w-xl md:p-0 md:py-[2.5rem] lg:max-w-lg xl:max-w-xl">
               {props.children}
             </main>
             <Aside />
